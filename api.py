1a89faaabe18f18c4333e38e18307e742f2d6f07
Glaysa <glaysa.df@gmail.com>
2021-04-01 20:01:39 +0200

    implement crud methods in the resoures class
    used a dummy to test the methods

--- api.py
+++ api.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restful import Api, Resource, abort
+import dummy_data
 
 app = Flask(__name__)
 api = Api(app)
 
-users = {}
-rooms = {}
+users = dummy_data.users
+rooms = dummy_data.rooms
 
 
 def abort_if_obj_doesnt_exist(obj_id: int, obj_list: iter, abort_message: str):
@@ -22,27 +23,33 @@ def abort_if_obj_exist(obj_id: int, obj_list: iter, abort_message: str):
 class User(Resource):
 
     def get(self, user_id: int = None):
-        pass
+        abort_if_obj_doesnt_exist(user_id, users, "User not found")
+        return users[user_id]
 
     def delete(self, user_id: int = None):
-        pass
+        abort_if_obj_doesnt_exist(user_id, users, "User not found")
+        deleted_user = users.pop(user_id)
+        return {"deleted user": deleted_user}
 
 
 # Shows a single room
 class Room(Resource):
 
     def get(self, room_id: int = None):
-        pass
+        abort_if_obj_doesnt_exist(room_id, rooms, "Room not found")
+        return rooms[room_id]
 
     def delete(self, room_id: int = None):
-        pass
+        abort_if_obj_doesnt_exist(room_id, rooms, "Room not found")
+        deleted_room = rooms.pop(room_id)
+        return {"deleted room": deleted_room}
 
 
 # Shows a list of users
 class UserList(Resource):
 
     def get(self):
-        pass
+        return users
 
     def post(self):
         pass
@@ -52,7 +59,7 @@ class UserList(Resource):
 class RoomList(Resource):
 
     def get(self):
-        pass
+        return rooms
 
     def post(self):
         pass
@@ -62,7 +69,8 @@ class RoomList(Resource):
 class RoomUserList(Resource):
 
     def get(self, room_id: int):
-        pass
+        abort_if_obj_doesnt_exist(room_id, rooms, "Room not found")
+        return rooms[room_id]["users"]
 
     def post(self, room_id: int):
         pass
@@ -72,14 +80,23 @@ class RoomUserList(Resource):
 class RoomMessageList(Resource):
 
     def get(self, room_id: int):
-        pass
+        abort_if_obj_doesnt_exist(room_id, rooms, "Room not found")
+        return rooms[room_id]["messages"]
 
 
 # Shows a list of messages from a user in a specific room
 class RoomUserMessageList(Resource):
 
     def get(self, room_id: int, user_id: int):
-        pass
+        abort_if_obj_doesnt_exist(room_id, rooms, "Room not found")
+        abort_if_obj_doesnt_exist(user_id, users, "User not found")
+        room_message_list = rooms[room_id]["messages"]
+        room_user_list = rooms[room_id]["users"]
+        abort_if_obj_doesnt_exist(user_id, room_user_list, "User is not a part of this room")
+        for message in room_message_list:
+            if message["sender"] == users[user_id]:
+                return message
+        return {"message": "This user has not sent a message to this room yet."}
 
     def post(self, room_id: int, user_id: int):
         pass
@@ -90,9 +107,9 @@ api.add_resource(User, "/api/user/<int:user_id>")
 api.add_resource(Room, "/api/room/<int:room_id>")
 api.add_resource(UserList, "/api/users")
 api.add_resource(RoomList, "/api/rooms")
-api.add_resource(RoomUserList, "/api/room/<room_id:int>/users")
-api.add_resource(RoomMessageList, "/api/room/<room_id:int>/messages")
-api.add_resource(RoomUserMessageList, "/api/room/<room_id:int>/<user_id:int>/messages")
+api.add_resource(RoomUserList, "/api/room/<int:room_id>/users")
+api.add_resource(RoomMessageList, "/api/room/<int:room_id>/messages")
+api.add_resource(RoomUserMessageList, "/api/room/<int:room_id>/<int:user_id>/messages")
 
 
 if __name__ == "__main__":
