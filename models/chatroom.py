c2ef595146ab9af2c6f8c5efede985fb7e1f5a9c
NimaAB <nimabewrani98@gmail.com>
2021-04-02 22:21:41 +0200

    added id property to models

--- models/chatroom.py
+++ models/chatroom.py
@@ -1,7 +1,18 @@
 class Chatroom:
+    _ROOM_ID = 1
+
     def __init__(self, name):
+        self.id = self._ROOM_ID
+        self.__class__._ROOM_ID += 1
         self.name = name
         self.users = []
 
     def set_user(self, user):
         self.users.append(user)
+
+    def __str__(self):
+        return f"{{\n" \
+               f"\t\"id\" : \"{self.id}\",\n" \
+               f"\t\"name\" : \"{self.name}\",\n" \
+               f"\t\"users\" : \"{self.users}\",\n" \
+               f"}}"
