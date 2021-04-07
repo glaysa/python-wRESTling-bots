aef443f76b4cbb4084e1a3e1508a201d71658ae2
NimaAB <nimabewrani98@gmail.com>
2021-04-07 23:49:30 +0200

    added login page and stylet both login.html and register.html

--- app/web_views.py
+++ app/web_views.py
@@ -5,19 +5,23 @@ from validation.form_validations import EnterAppForm, CreateRoomForm
 app.config['SECRET_KEY'] = '354b9b92b934613f14afe99c94a82415a9a3d49a'  # Not Necessary
 
 
-@app.route("/register")
-def get_login():
-    return render_template("register.html")
-
-
 @app.route("/")
+@app.route("/login")
+def get_login_page():
+    return render_template("login.html", msg="Username not found in our users list")
+
+
+@app.route("/register")
+def get_register():
+    return render_template("register.html",msg="will send a message here from the /api/users (post)")
+
+
 @app.route("/home")
 def get_home():
-    return render_template("home.html",)
+    # msg = request.args.get('msg')
+    return render_template("home.html")
 
 
-'''
 @app.route("/chatroom")
 def get_room():
     return render_template("chatroom.html")
-'''
