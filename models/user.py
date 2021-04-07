8a1a729c35aed909ca28ce985935c9e306ede68c
Glaysa <glaysa.df@gmail.com>
2021-04-07 23:30:27 +0200

    forsøk på post problem
    endret litt på home.html

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
-import json
-
 
 class User:
+
     _USER_ID = 1
 
     def __init__(self, username, personality):
@@ -11,14 +10,16 @@ class User:
         self.personality = personality
 
     @classmethod
-    def json_to_user(cls, json_str):
-        dictionary = json.loads(json_str)
-        return cls(**dictionary)
+    def json_to_user(cls, username, personality):
+        # dictionary = json.loads(json_str)
+        # return cls(**dictionary)
+        user = cls(username, personality)
+        return User.to_dictionary(user)
 
     @staticmethod
     def to_dictionary(obj):
         return {
-            'usr_id': obj.usr_id,
-            'username': obj.username,
-            'personality': obj.personality
+            "user_id": obj.usr_id,
+            "username": obj.username,
+            "personality": obj.personality
         }
