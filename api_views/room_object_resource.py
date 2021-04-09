50e063a64081af0070f81923cc380474b94a4cc8
Glaysa <glaysa.df@gmail.com>
2021-04-09 22:42:08 +0200

    user can join a room

--- api_views/room_object_resource.py
+++ api_views/room_object_resource.py
@@ -1,21 +1,36 @@
+from dataclasses import asdict
+
+from flask import session, redirect, url_for
 from flask_restful import Resource
+from api_views import room_list
 
 
 # Shows a list of users from a specific room
 class RoomUserList(Resource):
 
-    def get(self, room_id: int):
-        pass
+    def get(self, room_id: str):
+        for room in room_list:
+            if room.room_id == room_id:
+                return room.users
+        return {"message": "Room not found"}, 404
 
-    def post(self, room_id: int):
-        pass
+    def post(self, room_id: str):
+        for room in room_list:
+            if room.room_id == room_id:
+                user = session['user']
+                room.users.append(user)
+                return redirect(url_for('get_home'))
+        return {"message": "Room not found"}, 404
 
 
 # Shows a list of messages from a specific room
 class RoomMessageList(Resource):
 
     def get(self, room_id: int):
-        pass
+        for room in room_list:
+            if room.room_id == room_id:
+                return room.messages
+        return {"message": "Room not found"}, 404
 
 
 # Shows a list of messages from a user in a specific room
