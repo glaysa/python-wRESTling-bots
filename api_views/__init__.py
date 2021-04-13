b7cbc28956db5d9198f087739eeca7b50e5482ae
NimaAB <nimabewrani98@gmail.com>
2021-04-13 13:06:34 +0200

    added four users

--- api_views/__init__.py
+++ api_views/__init__.py
@@ -2,8 +2,11 @@ from typing import List
 
 from data.models import User, Chatroom as Room
 
-# test user:
-user1 = User(username="Test_user", personality="Sweet")
+# test users
+user1 = User(username="Alex", personality="grumpy")
+user2 = User(username="Helena", personality="annoying")
+user3 = User(username="Edward", personality="sweet")
+user4 = User(username="Anna", personality="energetic")
 
-user_list: List[User] = [user1]
+user_list: List[User] = [user1, user2, user3, user4]
 room_list: List[Room] = []
