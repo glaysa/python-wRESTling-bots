b8cccb04734d2527403984ee2413b19b14c6332b
NimaAB <nimabewrani98@gmail.com>
2021-04-16 12:50:21 +0200

    har fikset et feil i bot filen

--- bot/bots.py
+++ bot/bots.py
@@ -14,18 +14,18 @@ def energetic(message=None) -> Message:
     if message:
         return responses.choose_energetic_response(message)
     else:
-        return Message(sender=user3, content=Content(message="Let's party guys! My treat!"))
+        return Message(sender=user4, content=Content(message="Let's party guys! My treat!"))
 
 
 def annoying(message=None) -> Message:
     if message:
         return responses.choose_annoying_response(message)
     else:
-        return Message(sender=user3, content=Content(message="It's time to steal something. You guys in?"))
+        return Message(sender=user2, content=Content(message="It's time to steal something. You guys in?"))
 
 
 def grumpy(message=None) -> Message:
     if message:
         return responses.choose_grumpy_response(message)
     else:
-        return Message(sender=user3, content=Content(message="Let's sleep at my house. You coming or what?"))
+        return Message(sender=user1, content=Content(message="Let's sleep at my house. You coming or what?"))
